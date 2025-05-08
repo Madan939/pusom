d4c6c6a711cdfd482582b2e0c31826ed848dab1e
Madan Golay Tamang <madantamang939@gmail.com>
2025-05-09 01:51:24 +0545

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Home = () => {
+  useEffect(()=>{
+document.title="PUSOM | Purbanchal University School of Management"
+  },[]);
   return (
     <>
     <p>Home</p>
